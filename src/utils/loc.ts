5d2cf9d521b55bc40dc4ade987acf4836f8bc149
David <dkanekanian1@gmail.com>
2020-06-04 18:51:55 +0100

    Add loc object constructor overload

--- src/utils/loc.ts
+++ src/utils/loc.ts
@@ -6,14 +6,31 @@
  */
 export class Loc extends THREE.Vector3 {
 
-    public constructor(coordinates: Array<Number>);
+    /** Sets X, Y, Z elements to zero. */
+    public constructor();
+
+    /** Constructs X, Y, Z elements individually. */
     public constructor(x: Number, y: Number, z: Number);
+    
+    /** Constructs X and Y elements to simulate a 2D vector. (Z will be zero.) */
+    public constructor(x: Number, y: Number);
+
+    /** Copy constructor. Sets X, Y, Z from Loc or array's elements. */
+    public constructor(other: Loc | Array<Number>);
 
     public constructor() {
         if (arguments[0] instanceof Array)
             super(...arguments[0]);
+        else if (arguments[0] instanceof Loc)
+            super(...arguments[0]);
         else
             super(...arguments);
+    }
+
+    public *[Symbol.iterator]() {
+        yield this.x;
+        yield this.y;
+        yield this.z;
     }
 
     public toString(): string {
@@ -23,7 +40,7 @@ export class Loc extends THREE.Vector3 {
     /** Return Loc with types (int or float) from repr string OTHER.
      * Example input is `(10.0, 5.0)`, not `(X=2, Y=30)`
      */
-    public fromStr(other: string): Loc {
+    public static fromStr(other: string): Loc {
         other = other.trim();
         while (other.includes(" ")) other = other.replace(" ", "");
         other = other.replace("(", "").replace(")", "");
@@ -31,3 +48,21 @@ export class Loc extends THREE.Vector3 {
         return new Loc(Number(items[0]), Number(items[1]), Number(items[2]));
     }
 }
+
+/** Allows arrays to be passed as Loc.
+ * 
+ * You should still construct a Loc out of the parameter passed in as ILoc
+ * to be sure it is an actual Loc object. This creates a copy if a Loc was
+ * already passed in, so only use sparingly!
+ * 
+ * Example:
+ * ```
+ * fn(location: ILoc) {
+ *     let l = new Loc(location); // Ensure it is a Loc.
+ *     // code...
+ * }
+ * fn(new Loc(1, 2, 3));
+ * fn([1, 2, 3]);
+ * ```
+ */
+export type ILoc = Loc | Array<Number>;
